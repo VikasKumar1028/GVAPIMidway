402a3a9535a7149baff47b0a9d793b75657ccc97
Vinay Kumar Sumani <vs00419238@techmahindra.com>
2016-04-06 14:43:55 +0530

    Vinay Deactivate Device Changes
    Vinay Deactivate Device Changes

--- src/main/java/com/gv/midway/pojo/deactivateDevice/request/DeactivateDevices.java
+++ src/main/java/com/gv/midway/pojo/deactivateDevice/request/DeactivateDevices.java
@@ -4,25 +4,21 @@ import java.util.Arrays;
 
 public class DeactivateDevices {
 	
-	private DeactivateDeviceId[] deactivateDeviceIds;
+	private DeactivateDeviceId[] deviceIds;
 
-	public DeactivateDeviceId[] getActivateDeviceIds() {
-		return deactivateDeviceIds;
+	public DeactivateDeviceId[] getDeviceIds() {
+		return deviceIds;
 	}
 
-	public DeactivateDeviceId[] getDeactivateDeviceIds() {
-		return deactivateDeviceIds;
-	}
-
-	public void setDeactivateDeviceIds(DeactivateDeviceId[] deactivateDeviceIds) {
-		this.deactivateDeviceIds = deactivateDeviceIds;
+	public void setDeviceIds(DeactivateDeviceId[] deviceIds) {
+		this.deviceIds = deviceIds;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + Arrays.hashCode(deactivateDeviceIds);
+		result = prime * result + Arrays.hashCode(deviceIds);
 		return result;
 	}
 
@@ -35,19 +31,17 @@ public class DeactivateDevices {
 		if (getClass() != obj.getClass())
 			return false;
 		DeactivateDevices other = (DeactivateDevices) obj;
-		if (!Arrays.equals(deactivateDeviceIds, other.deactivateDeviceIds))
+		if (!Arrays.equals(deviceIds, other.deviceIds))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("ActivateDevices [deactivateDeviceIds=");
-		builder.append(Arrays.toString(deactivateDeviceIds));
-		builder.append("]");
-		return builder.toString();
+		return "DeactivateDevices [deviceIds=" + Arrays.toString(deviceIds)
+				+ "]";
 	}
+
 
 	
 	
