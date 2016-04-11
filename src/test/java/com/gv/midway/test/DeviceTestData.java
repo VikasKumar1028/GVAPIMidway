348e123409246e4a78f2da6b190a233e907fd575
SY00453608 <SY00453608@techmahindra.com>
2016-04-11 12:34:04 +0530

    Fixed Junit
    Junit Test Cases

--- src/test/java/com/gv/midway/test/DeviceTestData.java
+++ src/test/java/com/gv/midway/test/DeviceTestData.java
@@ -2,26 +2,32 @@ package com.gv.midway.test;
 
 import org.apache.camel.Exchange;
 
-
-
-
 import com.gv.midway.pojo.Header;
 import com.gv.midway.pojo.activateDevice.request.ActivateDeviceId;
 import com.gv.midway.pojo.activateDevice.request.ActivateDeviceRequest;
 import com.gv.midway.pojo.activateDevice.request.ActivateDeviceRequestDataArea;
+import com.gv.midway.pojo.activateDevice.request.ActivateDevices;
+import com.gv.midway.pojo.activateDevice.response.ActivateDeviceResponse;
+import com.gv.midway.pojo.callback.request.CallBackVerizonRequest;
+import com.gv.midway.pojo.deactivateDevice.request.DeactivateDeviceId;
 import com.gv.midway.pojo.deactivateDevice.request.DeactivateDeviceRequest;
 import com.gv.midway.pojo.deactivateDevice.request.DeactivateDeviceRequestDataArea;
+import com.gv.midway.pojo.deactivateDevice.request.DeactivateDevices;
+import com.gv.midway.pojo.device.request.DeviceDataArea;
 import com.gv.midway.pojo.deviceInformation.request.DeviceInformationRequest;
 import com.gv.midway.pojo.deviceInformation.request.DeviceInformationRequestDataArea;
-//import com.gv.midway.pojo.DeviceId;
-import com.gv.midway.pojo.deviceInformation.request.DeviceInformationRequest;
+import com.gv.midway.pojo.deviceInformation.response.Cell;
+import com.gv.midway.pojo.deviceInformation.response.DeviceInformation;
+import com.gv.midway.pojo.verizon.DeviceId;
 
 public class DeviceTestData extends Header {
-   /* protected ActivateDeviceRequest expectHeader;
+    protected ActivateDeviceRequest expectHeader;
+    protected ActivateDeviceRequest expectedActivateDeviceRequest;
     protected DeviceInformationRequest expectDeviceInfo;
     protected DeactivateDeviceRequest expectDeactiveDevice;
+    protected ActivateDeviceResponse  expectedActivateDeviceResponse;
+    protected CallBackVerizonRequest expectedActivateCallback;
     Exchange exchange;
-    //protected Header deliveredDrinks;
     Header header =new Header();    	
     public void setExpectedHeader(ActivateDeviceRequest activateDeviceRequest) {
         this.expectHeader = activateDeviceRequest;
@@ -36,15 +42,15 @@ public class DeviceTestData extends Header {
    		   header.setSourceName("KORE");
    		   header.setTransactionId("cde2131ksjd");
    		   header.setBsCarrier("KORE"); 
-    	   
+    	   header.setRegion("USA");
     	   req.setHeader(header);   
     	  
-    	    ActivateDeviceRequest request = (ActivateDeviceRequest) exchange
-    				.getIn().getBody(ActivateDeviceRequest.class);
+    	    /*ActivateDeviceRequest request = (ActivateDeviceRequest) exchange
+    				.getIn().getBody(ActivateDeviceRequest.class);*/
     	   
     	    
     	     System.out.println("Expected Header----------------" +expectHeader.getHeader()); 
-    	     System.out.println("Delivered Header----------------" +req.getHeader()); 
+    	     System.out.println("Delivered Request is----------------" +req.getHeader()); 
     	     
     	    //check for different no. of and value of parameters
     	     if(!expectHeader.getHeader().equals(req.getHeader())){
@@ -83,40 +89,42 @@ public class DeviceTestData extends Header {
 		// TODO Auto-generated method stub
 		DeviceInformationRequest req=new DeviceInformationRequest();
 		DeviceInformationRequestDataArea dataArea=new DeviceInformationRequestDataArea();
-		DeviceId deviceIds = new DeviceId();
- 		deviceIds.setId("");
+		
+		DeviceInformation deviceInformation = new DeviceInformation();
+ 		deviceInformation.setAccountName("Test");
  		
- 		DeviceId[] deviceIdsArray = { deviceIds };
-
- 		//dataArea.setDeviceId(deviceIdsArray);
+		DeviceDataArea deviceDataArea = new DeviceDataArea();
+		deviceDataArea.setDevices(deviceInformation);
+		
+ 		DeviceId deviceId = new DeviceId();
+ 		deviceId.setId("01");
+ 		deviceId.setKind("k01");
  		
- 		dataArea.setAccountName("Test");
+ 		dataArea.setDeviceId(deviceId);
+ 		
+ 		
         req.setDataArea(dataArea);
-        dataArea.setAccountName("Test");
-        req.setDataArea(dataArea);
-       
- 		header.setSourceName("KORE"); 				
+     
+      	header.setSourceName("KORE"); 				
  		 req.setHeader(header);
  		 
- 	   System.out.println("Delivered Source Name----------------" +req.getHeader().getSourceName()); 
-	     System.out.println("Delivered Account Name----------------" +req.getDataArea().getDeviceId().length); 
-	      
-	     //null pointer check for source name and account name
-	     if(req.getHeader().getSourceName()==null || req.getDataArea().getAccountName()==null || req.getDataArea().getAccountName().equals("")  || req.getDataArea().getAccountName().isEmpty()){
-	    	 throw new AssertionError("Source Name and Account Name can not be Blank");
-	     }
-	     
+ 	   System.out.println("Delivered Request  Name is.....----------------" +req.getDataArea().getDeviceId().getId()); 
+	  
 	   //null pointer check for device Id
-	     if(deviceIds.getId()==null || deviceIds.getId().equals("") || deviceIds.getId().isEmpty())
+	    if(req.getDataArea().getDeviceId().getId()==null || req.getDataArea().getDeviceId().getId().equals("") || req.getDataArea().getDeviceId().getId().isEmpty())
 	     {
 	    	 throw new AssertionError("Device Id can not be Blank");	 
 	     }
+ 	   if(!expectDeviceInfo.getDataArea().getDeviceId().getId().equals(req.getDataArea().getDeviceId().getId())){
+ 		  throw new AssertionError("Invalid Device Id");
+ 	   }
+ 	   
 	}
 
 	//set expected dummy data to Activate Device request
 	public void setExpectedActivateDevice(ActivateDeviceRequest activateDeviceRequest) {
 		// TODO Auto-generated method stub
-		this.expectHeader = activateDeviceRequest;
+		this.expectedActivateDeviceRequest = activateDeviceRequest;
 	}
 
 	//test  case for Activate device with null EAP Code
@@ -125,19 +133,26 @@ public class DeviceTestData extends Header {
 		ActivateDeviceRequest req=new ActivateDeviceRequest();
 		ActivateDeviceRequestDataArea dataArea=new ActivateDeviceRequestDataArea();
 	
+		ActivateDevices [] decs=new ActivateDevices[1];
+		ActivateDevices adevices = new ActivateDevices();
+		
+		
+		ActivateDeviceId[] ActivateDeviceIdArray = new ActivateDeviceId[1];
+		 
+		ActivateDeviceId deviceId= new ActivateDeviceId();
+		
+		deviceId.setId("89014103277405946190");
+		deviceId.setKind("ghgjg");
+		deviceId.seteAPCode("eAPCode");
+		
+
+		ActivateDeviceIdArray[0] = deviceId;
+		
+		adevices.setDeviceIds(ActivateDeviceIdArray);
+		decs[0] = adevices;
+		dataArea.setDevices(decs);
+		req.setDataArea(dataArea);
     
-		ActivateDeviceId [] deviceId = new ActivateDeviceId[1];
-        deviceId[0] = new ActivateDeviceId();
-    	deviceId[0].setId("89014103277405946190");
-		deviceId[0].setKind("kind");
-		deviceId[0].seteAPCode("eAPCode");
-		
-		
-		dataArea.setDeviceId(deviceId);
- 		
- 		
-         req.setDataArea(dataArea);
-        
   		 header.setSourceName("KORE");
   		 req.setHeader(header);
   		 
@@ -145,10 +160,13 @@ public class DeviceTestData extends Header {
 	     System.out.println("Delivered Source Name----------------" +req.getHeader().getSourceName()); 
 	      
 	     //null pointer check for EAP Code if Source is 'KORE'
-	     if(req.getHeader().getSourceName()!=null && req.getHeader().getSourceName().equalsIgnoreCase("KORE") && req.getDataArea().getDeviceId()[0].geteAPCode().isEmpty()){
+	     if(req.getHeader().getSourceName()!=null && req.getHeader().getSourceName().equalsIgnoreCase("KORE") && req.getDataArea().getDevices()[0].getDeviceIds()[0].geteAPCode().isEmpty()){
 	    	 throw new AssertionError("EAP Code is Mandatory for KORE");
 	     }
-			     
+			  
+	     if(!expectedActivateDeviceRequest.getHeader().getSourceName().equals(req.getHeader().getSourceName())){
+	    	 throw new AssertionError("Source Name is not correct");
+	     }
 			  
 	}
 
@@ -165,31 +183,136 @@ public class DeviceTestData extends Header {
 				DeactivateDeviceRequest req=new DeactivateDeviceRequest();
 				DeactivateDeviceRequestDataArea dataArea=new DeactivateDeviceRequestDataArea();
 			
-		   
-		        DeviceId[] deviceId = new DeviceId[1];
-		        deviceId[0] = new DeviceId();
-		    	//deviceId[0].setId("89014103277405946190");
-				deviceId[0].setKind("ghgjg");
-				
-				dataArea.setDeviceId(deviceId);
-		 		dataArea.setAccountName("Test");
+				DeactivateDevices [] deDevices=new DeactivateDevices[1];
+		        DeactivateDevices ddevices = new DeactivateDevices();
+
+		        DeactivateDeviceId[] DeActivateDeviceIdArray = new DeactivateDeviceId[1];
+		 		 
+		 		DeactivateDeviceId deactivateDeviceId= new DeactivateDeviceId();
 		 		
-		         req.setDataArea(dataArea);
-		        
+		 		    deactivateDeviceId.setId("89014103277405946190");
+		 		deactivateDeviceId.setKind("ghgjg");
+		 		
+		 		DeActivateDeviceIdArray[0] = deactivateDeviceId;
+		 		
+		 		ddevices.setDeviceIds(DeActivateDeviceIdArray);
+		 		deDevices[0] = ddevices;
+		 		dataArea.setDevices(deDevices);
+		 		dataArea.setFlagScrap(Boolean.FALSE);
+		 		req.setDataArea(dataArea);
+		         
 		  		 header.setSourceName("KORE");
 		  		 req.setHeader(header);
 		  		 
 		  				
 			     System.out.println("Delivered Source Name----------------" +req.getHeader().getSourceName()); 
-			     System.out.println("Delivered Account Name----------------" +req.getDataArea().getAccountName()); 
-			      
-			     //null pointer check for deviceId
-			     if(req.getDataArea().getDeviceId()[0].getId()==null || req.getDataArea().getDeviceId()[0].getId().equals("") ){
+			   
+			     //null pointer check for Flag Scrap
+			     if(req.getDataArea().getFlagScrap()==null){
+			    	 throw new AssertionError("Flag Scrap is Mandatory");
+			     }
+					
+			   //null pointer check for device Id
+			     if(req.getDataArea().getDevices()[0].getDeviceIds()[0].getId()==null || req.getDataArea().getDevices()[0].getDeviceIds()[0].getId().isEmpty()){
 			    	 throw new AssertionError("Device Id is Mandatory");
 			     }
-					  				
+			     
+			     if(!expectDeactiveDevice.getDataArea().getDevices()[0].getDeviceIds()[0].getId().equals(req.getDataArea().getDevices()[0].getDeviceIds()[0].getId()))
+			     {	
+			    	 throw new AssertionError("Device Id is Different");
+			     }
 	}
 
-	*/
+	public void setExpectedActivateDeviceResponse(ActivateDeviceResponse activateDeviceResponse) {
+		// TODO Auto-generated method stub
+		this.expectedActivateDeviceResponse=activateDeviceResponse;
+	}
+	
+	//test case for Activate Device Response
+	public void verifyDeviceActivationResponseData(ActivateDeviceResponse response) {
+		// TODO Auto-generated method stub
+		ActivateDeviceRequest req=new ActivateDeviceRequest();
+		ActivateDeviceRequestDataArea dataArea=new ActivateDeviceRequestDataArea();
+	
+    
+	     System.out.println("Delivered Order Number----------------" +response.getDataArea().getOrderNumber()); 
+	      
+	     //null pointer check for EAP Code if Source is 'KORE'
+	    if(expectedActivateDeviceResponse.getDataArea().getOrderNumber().equals(response.getDataArea().getOrderNumber())){
+	    	throw new AssertionError("Order Number is not correct");
+	    }
+	    
+  		 
+	}
 
+	public void setExpectedActivateCallback(
+			CallBackVerizonRequest callbackReq) {
+		// TODO Auto-generated method stub
+		this.expectedActivateCallback=callbackReq;
+	}
+
+	//for activate callback request
+	public void verifyActivateCallback() {
+		// TODO Auto-generated method stub
+		CallBackVerizonRequest callbackReq = new CallBackVerizonRequest();
+		
+		callbackReq.setRequestId("");
+		System.out.println("Callback Request Id:"+ callbackReq);
+		if(callbackReq.getRequestId()==null || callbackReq.getRequestId().isEmpty()){
+			throw new AssertionError("Request Id can not be null");
+		}
+	}
+
+	public void setExpectedCellInformation(Cell cellInfo) {
+		// TODO Auto-generated method stub
+		//this.expectedCellInfo=cellInfo;
+	}
+
+//test case for cell information
+	public void verifyCellInformation() {
+		// TODO Auto-generated method stub
+		Cell cell =new Cell();
+		cell.setEsn("ESN01");
+		cell.setMdn("MDN01");
+		cell.setSim("SIM01");
+		System.out.println("Request in Junit Test:" + cell);
+		if(cell.getEsn()==null || cell.getEsn().isEmpty()){
+			throw new AssertionError("Cell ESN No. can not be null");
+		}
+		
+		if(cell.getMdn()==null || cell.getMdn().isEmpty()){
+			throw new AssertionError("Cell MDN No. can not be null");
+		}
+		
+		if( cell.getSim()==null || cell.getSim().isEmpty()){
+			throw new AssertionError("Cell SIM No. can not be null");
+		}
+	}
+
+	public void setExpectedCellUpdateInformation(Cell cell) {
+		// TODO Auto-generated method stub
+		//this.expectedCellUpdateInfo=cell;
+	}
+
+	//Test case for cell Update
+	public void verifyCellUpdateInformation() {
+		// TODO Auto-generated method stub
+		Cell cell =new Cell();
+		cell.setEsn("ESN01");
+		cell.setMdn("MDN01");
+		cell.setSim("SIM01");
+		System.out.println("Request in Junit Test:" + cell);
+		if(cell.getEsn()==null || cell.getEsn().isEmpty()){
+			throw new AssertionError("Cell ESN No. can not be null");
+		}
+		
+		if(cell.getMdn()==null || cell.getMdn().isEmpty()){
+			throw new AssertionError("Cell MDN No. can not be null");
+		}
+		
+		if( cell.getSim()==null || cell.getSim().isEmpty()){
+			throw new AssertionError("Cell SIM No. can not be null");
+		}
+	}
+	
 }
--- src/test/java/com/gv/midway/test/MidwayJunitTest.java
+++ src/test/java/com/gv/midway/test/MidwayJunitTest.java
@@ -17,6 +17,7 @@
 package com.gv.midway.test;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.junit.After;
 import org.junit.Assert;
@@ -25,18 +26,28 @@ import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-//import com.gv.midway.pojo.DeviceId;
 import com.gv.midway.pojo.Header;
 import com.gv.midway.pojo.activateDevice.request.ActivateDeviceId;
 import com.gv.midway.pojo.activateDevice.request.ActivateDeviceRequest;
 import com.gv.midway.pojo.activateDevice.request.ActivateDeviceRequestDataArea;
+import com.gv.midway.pojo.activateDevice.request.ActivateDevices;
+import com.gv.midway.pojo.activateDevice.response.ActivateDeviceResponse;
+import com.gv.midway.pojo.activateDevice.response.ActivateDeviceResponseDataArea;
+import com.gv.midway.pojo.callback.request.CallBackVerizonRequest;
+import com.gv.midway.pojo.deactivateDevice.request.DeactivateDeviceId;
 import com.gv.midway.pojo.deactivateDevice.request.DeactivateDeviceRequest;
 import com.gv.midway.pojo.deactivateDevice.request.DeactivateDeviceRequestDataArea;
+import com.gv.midway.pojo.deactivateDevice.request.DeactivateDevices;
+import com.gv.midway.pojo.device.request.DeviceDataArea;
 import com.gv.midway.pojo.deviceInformation.request.DeviceInformationRequest;
 import com.gv.midway.pojo.deviceInformation.request.DeviceInformationRequestDataArea;
+import com.gv.midway.pojo.deviceInformation.response.Cell;
+import com.gv.midway.pojo.deviceInformation.response.DeviceInformation;
+import com.gv.midway.pojo.deviceInformation.response.DeviceInformationResponse;
+import com.gv.midway.pojo.verizon.DeviceId;
 
 public class MidwayJunitTest extends Assert {
-	/*private AbstractApplicationContext applicationContext;
+	private AbstractApplicationContext applicationContext;
 	private ProducerTemplate template;
 
 	protected DeviceTestData testData = new DeviceTestData();
@@ -50,7 +61,7 @@ public class MidwayJunitTest extends Assert {
 	}
 
 	// test case for header Information-common for all API
-	@Test
+	//@Test
 	public void testHeaderInformation() throws Exception {
 
 		Header header = new Header();
@@ -62,7 +73,8 @@ public class MidwayJunitTest extends Assert {
 		header.setSourceName("KORE");
 		header.setTransactionId("cde2131ksjd");
 		header.setBsCarrier("KORE");
-
+		header.setRegion("USA");
+		
 		req.setHeader(header);
 
 		// System.out.println("Request in Junit Test:"+req);
@@ -79,17 +91,29 @@ public class MidwayJunitTest extends Assert {
 		DeviceInformationRequest req = new DeviceInformationRequest();
 
 		DeviceInformationRequestDataArea dataArea = new DeviceInformationRequestDataArea();
-		DeviceId deviceIds = new DeviceId();
-		deviceIds.setId("89014103277405946190");
+		
+		//deviceIds.setId("89014103277405946190");
 
-		DeviceId[] deviceIdsArray = { deviceIds };
-
-		// dataArea.setDeviceId(deviceIdsArray);
-		dataArea.setAccountName("Test");
-		req.setDataArea(dataArea);
-
-		header.setSourceName("KORE");
-		req.setHeader(header);
+		DeviceInformation deviceInformation = new DeviceInformation();
+ 		deviceInformation.setAccountName("Test");
+ 		
+		DeviceDataArea deviceDataArea = new DeviceDataArea();
+		deviceDataArea.setDevices(deviceInformation);
+		
+ 		DeviceId deviceId = new DeviceId();
+ 		deviceId.setId("01");
+ 		deviceId.setKind("k01");
+ 		
+ 		dataArea.setDeviceId(deviceId);
+ 		
+ 		
+        req.setDataArea(dataArea);
+     
+        req.setDataArea(dataArea);
+       
+ 		header.setSourceName("KORE"); 				
+ 		 req.setHeader(header);
+ 		 
 
 		// System.out.println("Request in Junit Test:"+req);
 		testData.setExpectedDeviceInformation(req);
@@ -98,7 +122,7 @@ public class MidwayJunitTest extends Assert {
 	}
 
 	// test case for Activate Device
-	@Test
+	//@Test
 	public void testActivateDevice() throws Exception {
 
 		ActivateDeviceRequest req = new ActivateDeviceRequest();
@@ -106,13 +130,24 @@ public class MidwayJunitTest extends Assert {
 
 		ActivateDeviceRequestDataArea dataArea = new ActivateDeviceRequestDataArea();
 
-		ActivateDeviceId[] deviceId = new ActivateDeviceId[1];
-		deviceId[0] = new ActivateDeviceId();
-		deviceId[0].setId("89014103277405946190");
-		deviceId[0].setKind("ghgjg");
-		deviceId[0].seteAPCode("eAPCode");
-		dataArea.setDeviceId(deviceId);
+		ActivateDevices [] decs=new ActivateDevices[2];
+		ActivateDevices adevices = new ActivateDevices();
+		
+		
+		ActivateDeviceId[] ActivateDeviceIdArray = new ActivateDeviceId[1];
+		 
+		ActivateDeviceId deviceId= new ActivateDeviceId();
+		
+		deviceId.setId("89014103277405946190");
+		deviceId.setKind("ghgjg");
+		deviceId.seteAPCode("eAPCode");
+		
 
+		ActivateDeviceIdArray[0] = deviceId;
+		
+		adevices.setDeviceIds(ActivateDeviceIdArray);
+		decs[0] = adevices;
+		dataArea.setDevices(decs);
 		req.setDataArea(dataArea);
 
 		header.setSourceName("KORE");
@@ -126,7 +161,7 @@ public class MidwayJunitTest extends Assert {
 	}
 
 	// Test Case for Deactivate Device
-	@Test
+	//@Test
 	public void testDeactivateDevice() throws Exception {
 
 		DeactivateDeviceRequest req = new DeactivateDeviceRequest();
@@ -134,23 +169,110 @@ public class MidwayJunitTest extends Assert {
 
 		DeactivateDeviceRequestDataArea dataArea = new DeactivateDeviceRequestDataArea();
 
-		DeviceId[] deviceId = new DeviceId[1];
-		deviceId[0] = new DeviceId();
-		deviceId[0].setId("89014103277405946190");
-		deviceId[0].setKind("ghgjg");
+		DeactivateDevices [] deDevices=new DeactivateDevices[1];
+        DeactivateDevices ddevices = new DeactivateDevices();
 
-		dataArea.setDeviceId(deviceId);
-
-		req.setDataArea(dataArea);
-
-		header.setSourceName("KORE");
-		req.setHeader(header);
+        DeactivateDeviceId[] DeActivateDeviceIdArray = new DeactivateDeviceId[1];
+ 		 
+ 		DeactivateDeviceId deactivateDeviceId= new DeactivateDeviceId();
+ 		
+ 		deactivateDeviceId.setId("89014103277405946190");
+ 		deactivateDeviceId.setKind("ghgjg");
+ 		
+ 		DeActivateDeviceIdArray[0] = deactivateDeviceId;
+ 		
+ 		ddevices.setDeviceIds(DeActivateDeviceIdArray);
+ 		deDevices[0] = ddevices;
+ 		dataArea.setDevices(deDevices);
+ 		dataArea.setFlagScrap(Boolean.FALSE);
+ 		req.setDataArea(dataArea);
+         
+  		 header.setSourceName("KORE");
+  		 req.setHeader(header);
+  		 
 
 		System.out.println("Request in Junit Test:" + req);
 		testData.setExpectedDeactivateDevice(req);
 		template.sendBody("direct:deactivateDevice", req);
 		testData.verifyDeviceDeactivationData();
 
+	}
+	
+	//test case for Activate Device Response 
+	//@Test
+	public void testActivateDeviceResponse() throws Exception {
+
+		Exchange exchange;
+		Header header = new Header();
+		ActivateDeviceRequest req = new ActivateDeviceRequest();		
+		ActivateDeviceResponse activateDeviceResponse =new ActivateDeviceResponse();		
+		ActivateDeviceRequestDataArea dataArea = new ActivateDeviceRequestDataArea();
+		ActivateDeviceResponseDataArea responseDataArea =new ActivateDeviceResponseDataArea();
+		
+		responseDataArea.setOrderNumber("ORD001");
+		activateDeviceResponse.setDataArea(responseDataArea);	
+		req.setDataArea(dataArea);
+		header.setSourceName("KORE");
+		req.setHeader(header);
+
+		System.out.println("Request in Junit Test:" + activateDeviceResponse);
+		testData.setExpectedActivateDeviceResponse(activateDeviceResponse);
+		
+		//template.sendBody("direct:activateDevice", req);
+		ActivateDeviceResponse response = (ActivateDeviceResponse) template.requestBody(
+			      "direct:activateDevice", req);
+		DeviceInformationResponse  response1 =    (DeviceInformationResponse)template.requestBody(
+			      "direct:deviceInformation", req);
+		/*ActivateDeviceRequest request = (ActivateDeviceRequest) exchange
+				.getIn().getBody(ActivateDeviceRequest.class);
+		
+		*/
+		System.out.println("Rsponse in Junit Test:" + response);
+		System.out.println("Device Information Response in Junit Test................:" + response1);
+		testData.verifyDeviceActivationResponseData(response);
+
+	}	
+	
+	
+	//Test case for Activate callback 'Request Id'
+	//@Test
+	public void testActivateCallback() throws Exception {
+
+		CallBackVerizonRequest callbackReq = new CallBackVerizonRequest();
+	
+		callbackReq.setRequestId("R001");
+  		
+		System.out.println("Request in Junit Test:" + callbackReq);
+		testData.setExpectedActivateCallback(callbackReq);
+		template.sendBody("direct:callbacks", callbackReq);
+		testData.verifyActivateCallback();
+
+	}
+	
+	//Test case for Cell 
+	//@Test
+	public void testCell() throws Exception{
+		Cell cell =new Cell();
+		cell.setEsn("ESN01");
+		cell.setMdn("MDN01");
+		cell.setSim("SIM01");
+		System.out.println("Request in Junit Test:" + cell);
+		testData.setExpectedCellInformation(cell);
+		//template.sendBody("direct:cell", cell);
+		testData.verifyCellInformation();
+	}
+
+	//Test case for cell Update
+	//@Test
+	public void testCellUpdate() throws Exception{
+		Cell cell =new Cell();
+		cell.setEsn("ESN01");
+		cell.setMdn("MDN01");
+		cell.setSim("SIM01");
+		System.out.println("Request in Junit Test:" + cell);
+		testData.setExpectedCellUpdateInformation(cell);
+		//template.sendBody("direct:cell", cellUpdate);
+		testData.verifyCellUpdateInformation();
 	}
 
 	@After
@@ -162,6 +284,6 @@ public class MidwayJunitTest extends Assert {
 
 	protected CamelContext getCamelContext() throws Exception {
 		return applicationContext.getBean("camel", CamelContext.class);
-	}*/
+	}
 
 }
