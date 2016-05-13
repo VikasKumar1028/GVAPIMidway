a68feb25d2dd2f3d5aa3e884484ca355e845df0f
Sadhana12star <Sadhana.gurjar@techmahindra.com>
2016-05-13 15:32:54 +0530

    Fixed testing bugs
    Fixed testing bugs

--- src/main/java/com/gv/midway/processor/reactivate/StubKoreReactivateDeviceProcessor.java
+++ src/main/java/com/gv/midway/processor/reactivate/StubKoreReactivateDeviceProcessor.java
@@ -2,10 +2,50 @@ package com.gv.midway.processor.reactivate;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.log4j.Logger;
+
+import com.gv.midway.constant.IResponse;
+import com.gv.midway.pojo.Header;
+import com.gv.midway.pojo.Response;
+import com.gv.midway.pojo.reActivateDevice.response.ReactivateDeviceResponse;
+import com.gv.midway.pojo.reActivateDevice.response.ReactivateDeviceResponseDataArea;
 
 public class StubKoreReactivateDeviceProcessor implements Processor {
 
-	public void process(Exchange arg0) throws Exception {
+	Logger log = Logger.getLogger(KoreReactivateDevicePreProcessor.class
+			.getName());
+
+	public void process(Exchange exchange) throws Exception {
+
+		log.info("Begin:StubKoreReactivateDeviceProcessor");
+		ReactivateDeviceResponse reactivateDeviceResponse = new ReactivateDeviceResponse();
+
+		ReactivateDeviceResponseDataArea reactivateDeviceResponseDataArea = new ReactivateDeviceResponseDataArea();
+
+		Header responseheader = new Header();
+
+		Response response = new Response();
+
+		response.setResponseCode(IResponse.SUCCESS_CODE);
+
+		response.setResponseDescription("Device is reactivate successfully");
+		response.setResponseStatus("SUCESS");
+
+		responseheader.setApplicationName("WEB");
+		responseheader.setRegion("USA");
+		responseheader.setTimestamp("2016-03-08T21:49:45");
+		responseheader.setOrganization("Grant Victor");
+		responseheader.setSourceName("VERIZON");
+		responseheader.setTransactionId("cde2131ksjd");
+		responseheader.setBsCarrier("VERIZON");
+
+		reactivateDeviceResponse.setHeader(responseheader);
+		reactivateDeviceResponse.setResponse(response);
+		reactivateDeviceResponseDataArea.setOrderNumber("StubKoreReactivateDeviceProcessor");
+		reactivateDeviceResponse.setDataArea(reactivateDeviceResponseDataArea);
+
+		exchange.getIn().setBody(reactivateDeviceResponse);
+		log.info("End:StubKoreReactivateDeviceProcessor");
 
 	}
 
